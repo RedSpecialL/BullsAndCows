4ede9e55b7a02815b5221bccb4612042e9b89c88
a.gordienko <a.gordienko@avantize.com>
2016-12-22 14:35:05 +0200

    1. Renamed lib. 2. Implemented getter functions for the current and max turns.

--- modules/bnclib/include/Rules.h
+++ modules/bnclib/include/Rules.h
@@ -51,6 +51,18 @@ public:
 	 * Initialize members for the new game.
 	 */
 	void reset(const uint8_t maxTurns, const std::string& task);
+
+	/** Setters and getters.*/
+public:
+	/**
+	 * @return current turn.
+	 */
+	uint8_t getCurrentTurn() const;
+
+	/**
+	 * @return max turns.
+	 */
+	uint8_t getMaxTurns() const;
 	
 	/**
 	 * Private data members.
@@ -63,5 +75,19 @@ private:
 	/** Hidden word.*/
 	std::string m_task;
 };
+
+/**
+ * Inline implementations.
+ */
+
+inline uint8_t GameRules::getCurrentTurn() const
+{
+	return m_currentTurn;
+}
+
+inline uint8_t GameRules::getMaxTurns() const
+{
+	return m_maxTurns;
+}
 
 } // end of namespace bnc
